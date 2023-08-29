c6927ccfdc5d9bd8af668bf75eb0c6e80a9164aa
Leo Obay <leonardoobay@gmail.com>
2023-08-28 23:46:44 -0700

    Add files via upload

---
+++ Java/RomanToInteger/roman.java
@@ -0,0 +1,31 @@
+class Solution {
+    public int romanToInt(String s) {
+         Map<Character, Integer> dictionary = new HashMap<>();
+        dictionary.put('I', 1);
+        dictionary.put('V', 5);
+        dictionary.put('X', 10);
+        dictionary.put('L', 50);
+        dictionary.put('C', 100);
+        dictionary.put('D', 500);
+        dictionary.put('M', 1000);
+
+        int lengthofRoman = s.length();
+        int num = dictionary.get(s.charAt(lengthofRoman - 1));
+
+        
+        for (int i = lengthofRoman - 2; i >= 0; i--) {
+
+            if (dictionary.get(s.charAt(i)) >= dictionary.get(s.charAt(i + 1))) {
+                num += dictionary.get(s.charAt(i));
+            } else {
+                num -= dictionary.get(s.charAt(i));
+            }
+        }
+        return num;
+    }
+
+
+
+
+    
+}
